14410971013530cdf36c6eb3f4623209c23930bf
Ramirez Lautaro <lautiramirez@users.noreply.github.com>
2021-03-23 16:27:48 -0300

    Ejercicio 1
    Ejercicio 1 del Lab #1.

---
+++ Lab #1/eje1.py
@@ -0,0 +1,24 @@
+#   1.  Dadas 3 variables numericas x,y,z notar las diferencias entre:
+#   a)  x/y + zyx/(y + z)
+#   b)  x/y*zyx/(y*z)
+
+def ejercicio_a(x, y, z):
+    a = (x/y + z)
+    b = z/(y*z)
+    return a, b
+
+
+def ejercicio_b(x, y, z):
+    a = (x/y*z)
+    b = (x/(y*z))
+    return a, b
+
+x = float(input("Ingrese un valor para tu variable x: "))
+y = float(input("Ingrese un valor para tu variable y: "))
+z = float(input("Ingrese un valor para tu variable z: "))
+
+a = ejercicio_a(x, y, z)
+b = ejercicio_b(x, y, z)
+
+print("Las expresiones del ejercicio 1a) son: ", a[0], "y ", a[1])
+print("Las expresiones del ejercicio 1b) son: ", b[0], "y",  b[1])
